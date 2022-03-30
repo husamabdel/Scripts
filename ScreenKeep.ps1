e371c268f80ae656822f5304bb5a6df2836a8924
husamabdel <81593002+husamabdel@users.noreply.github.com>
2022-03-30 15:38:44 -0400

    Add files via upload

---
+++ ScreenKeep.ps1
@@ -0,0 +1,16 @@
+﻿########################
+#
+#       Screen Keeper
+#       @Author: Husam Abdalla
+#       Date: 03/29/2022
+#
+########################
+
+$obj = New-Object -ComObject WScript.shell
+
+while($true){
+    
+    $obj.sendKeys(' ')
+    sleep 1
+
+}
